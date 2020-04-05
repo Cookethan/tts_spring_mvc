957cb0082eee042eedd2bdaf31e5626eb71a9f39
cookethan <ethancook48@gmail.com>
2020-04-05 13:32:03 -0500

    initial commit

---
+++ src/main/java/com/journaldev/spring/model/User.java
@@ -0,0 +1,13 @@
+package com.journaldev.spring.model;
+
+public class User {
+	private String userName;
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public void setUserName(String name) {
+		this.userName = name;
+	}
+}	
